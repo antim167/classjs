aa0cdbc51bdac1a35d87c4428d5d09df85490a53
MANVENDRA SINGH <Manvendra Singh Songara>
2021-04-06 23:00:21 +0530

    he

--- exp_inh.js
+++ exp_inh.js
@@ -3,13 +3,14 @@ class A {
     //1.property
     name = 'an';
     //2.constructor
-     constructor(){
+     constructor(n){
+         this.name=n;
          this.getName();
      }
 
     //3.method
-    getName(n) {
-
+    getName() {
+        console.log("hello from a class");
         return this.name;
     }
 
@@ -21,14 +22,14 @@ class B extends A {
     //1.property
 
     //2.constructor
-     constructor(){
-         super();
+     constructor(n){
+         super(n);
          this.getName1();
      }
 
-    //3.method
+    //3.methods
     getName1() {
-
+      console.log("helo from b class");
         return this.name;
     }
 
--- imp_inh.js
+++ imp_inh.js
@@ -1,5 +1,7 @@
-let B = require('./exp_inh ');
-let obj = new B();
-console.log(B);
+const B = require("./exp_inh");
+
+let obj = new B("hello");
+//console.log(B);
+//console.log(obj);
 let z = obj.getName();
 console.log(z);
